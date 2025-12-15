84a102794a2224fdcb628e56d063f8b4efa73f15
Sevinch <nebfjw>
2025-12-15 21:34:20 +0500

    adding Student Proporty

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Management.Domain.Models;
 
-namespace Management.Domain.Models
-{
-    internal class Student
-    {
-    }
-}
+ public class Student
+ {
+    public int Id { get; set; }
+    public string FirstName {  get; set; }
+    public string LastName { get; set; }
+    public DateTime StudentBrtihday { get; set; }
+
+ }
